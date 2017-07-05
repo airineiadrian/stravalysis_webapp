dac23581d6c8a9e28660af1b5667528a9ac05b1a
zomertech <zomertech@n3plcpnl0113.prod.ams3.secureserver.net>
2017-07-05 05:35:22 -0700

    also added git pull in hooks

--- hooks/hook.php
+++ hooks/hook.php
@@ -2,7 +2,10 @@
 
 $postdata = file_get_contents("php://input");
 $json = json_decode($postdata);
-if($json->ref == 'refs/heads/master')
+if($json->ref == 'refs/heads/master') {
 	shell_exec('git pull origin master');
+	shell_exec('git pull');
+}
+
 ?>
 
