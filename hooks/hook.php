81cbdd97a8ace9d5326e3593b7291575e4059b99
airineiadrian <adi.airinei@gmail.com>
2017-07-06 01:43:49 -0700

    added git reset to hook.php

--- hooks/hook.php
+++ hooks/hook.php
@@ -3,6 +3,7 @@
 $postdata = file_get_contents("php://input");
 $json = json_decode($postdata);
 if($json->ref == 'refs/heads/master') {
+	shell_exec('git reset --hard HEAD');
 	shell_exec('git pull origin master');
 	shell_exec('git pull');
 }
